0ad9c4867b306ee088a3eb15616f60dd58f18558
Krishna <ramain77@gmail.com>
2021-10-03 17:38:27 +0700

    add(code): basic hello world lesson

---
+++ basic/01_hello_world/01_hello_world.go
@@ -0,0 +1,32 @@
+// Pada materi pertama ini, blok kode pada baris nomor 3 kita abaikan terlebih dahulu,
+// di materi selanjutnya kita akan eksplorasi maksud & fungsionalitas dari blok kode tersebut.
+package main
+
+// Pada materi pertama ini, blok kode pada baris nomor 7 kita abaikan terlebih dahulu,
+// di materi selanjutnya kita akan eksplorasi maksud & fungsionalitas dari blok kode tersebut.
+import "fmt"
+
+// Pada materi pertama ini, blok kode pada baris nomor 11 kita abaikan terlebih dahulu,
+// di materi selanjutnya kita akan eksplorasi maksud & fungsionalitas dari blok kode tersebut.
+func main() {
+	//
+	// Kode dibawah ini merupakan perintah untuk menampilkan sebuah tulisan ke Stdout,
+	// apakah itu Stdout? Stdout atau yang berkepanjangan Standard Output secara singkat
+	// adalah tempat keluaran/output dari suatu perintah sebagai contoh perintah di baris nomor 20.
+	// Lalu dimanakah output yang di maksud? Output yang di maksud adalah program/aplikasi berbasis
+	// command line atau dikenal dengan istilah terminal dan mungkin istilah lain yang tidak asing yaitu
+	// Command Prompt/CMD. Untuk menjalankan perintah yang di maksud kita perlu melakukan
+	// sebuah aksi KOMPILASI di terminal atau CMD untuk menjalankan keseluruhan program ini.
+	fmt.Println("Hello world")
+
+	// Hasil yang ditampilkan setelah melakukan kompilasi & menjalankan program ini, terminal atau
+	// CMD akan menampilkan sebuah tulisan yaitu Hello world.
+
+	// Apakah hanya Hello world yang bisa ditampilkan? Tentu tidak, jikalau ingin menampilkan yang
+	// lain, kita bisa menuliskan kata atau kalimat di dalam nya sebagai contoh dibawah ini.
+	fmt.Println("Welcome to Bellshade")
+
+	// Syarat yang perlu di ingat adalah kata atau kalimat yang di deklarasi wajib berada di antara tanda petik dua.
+
+	// Happy learning see you next lesson!
+}
