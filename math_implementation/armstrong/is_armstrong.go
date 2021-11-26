d69ede7a8e82c77d9c47a3fe5c7070a105bf6634
slowy07 <slowy.arfy@gmail.com>
2021-11-26 22:09:31 +0700

    feat: testing angka armstrong
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ math_implementation/armstrong/is_armstrong.go
@@ -0,0 +1,36 @@
+// angka armstrong
+// memeriksa apakah nomor asyang diberikan adalah nomor armstrong
+// atau bukan
+// penjelasan tentang angka armstrong
+// https://medium.com/@achmadg6/apa-itu-armstrong-number-c60af4693ae
+
+package armstrong
+
+import (
+  "math"
+  "strconv"
+)
+
+func cekArmstrong(angka int) bool{
+  var digit_kanan int
+  var sum int = 0
+  var tempNum int = angka 
+
+  // untuk mendapatkan jumlah digit
+  // di dalam nomor
+  length := float64(len(strconv.Itoa(angka)))
+
+  // dapatkan digit paling kanan dan putuskan loop
+  // setelah semua digit diulang
+
+  for tempNum > 0 {
+    digit_kanan = tempNum % 10
+    sum += int(math.Pow(float64(digit_kanan), length))
+
+    // perbarui digit input dikurangi yang diproses
+    // paling kanan
+    tempNum /= 10
+  }
+  
+  return angka == sum
+}
