fc8bd30568771f5a464a9e8d7e288fa3ba0bacb2
slowy07 <slowy.arfy@gmail.com>
2021-11-26 19:33:12 +0700

    feat: menambahkan materi operator
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

---
+++ basic/05_operator/operator.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main(){
+  // membuat variabel a dan b
+  var a bool = true
+  var b bool = false
+
+  // membuat instruksi logika
+  if (a && b){
+    fmt.Printf("kondisi a dan b adalah benar")
+  }
+  if (a || b){
+    fmt.Printf("kondisi salah satu benar")
+  }
+
+  // mengganti value a dan b
+  a = false
+  b = true
+
+  // membuat instruksi logika
+  if (a && b){
+    fmt.Printf("kondisi a dan b adalah benar")
+  }
+  else{
+    fmt.Printf("tidak benar")
+  }
+  if(!(a && b)){
+    fmt.Printf("kondisi benar")
+  }
+}
+
