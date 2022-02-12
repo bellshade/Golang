572df03ac2756dcbb115a3982c4cc86569fc5b56
Yosep <43840441+yossdev@users.noreply.github.com>
2022-02-12 10:19:29 +0700

    Basic/06 looping (#16)
    * feat: menambahkan materi looping
    
    * docs: melengkapi materi looping

---
+++ basic/06_looping/recursion.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int = 3
+	fmt.Printf("%d! = %d", n, factorial_number(n))
+}
+
+// fungsi factorial_number akan mencari hasil faktorial dari
+// sebuah bilangan dengan cara perulangan recursive
+func factorial_number(n int) int {
+	// print stack (perulangan) fungsi
+	fmt.Printf("bilangan faktorial %d\n", n)
+	
+	// jika n atau parameter sama dengan 0
+	// tidak ada perulangan lagi (fungsi telah selesai dijalankan)
+	if n == 0 {
+		return 1
+	}
+
+	// memanggil kembali fungsi factorial_number
+	// dikarenakan nilai n belum sama dengan 0
+	return n * factorial_number(n-1)
+}
+
