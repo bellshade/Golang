0a484671e3a1ac18a6717e7f3e97592b5a135776
Badar Wildani <wildanie12@gmail.com>
2024-02-10 18:35:19 +0700

    style: gofmt-ed slice demo

--- basic/13_slice/slice.go
+++ basic/13_slice/slice.go
@@ -2,28 +2,28 @@ package main
 
 import "fmt"
 
-func main(){
-  /* membuat slice */
-  angka := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
-  printSlice(angka)
+func main() {
+	/* membuat slice */
+	angka := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	printSlice(angka)
 
-  /* print original slice */
-  fmt.Println("angka ==", angka)
-  
-  /* print dari subslice yng dimulai dari index 1 */
+	/* print original slice */
+	fmt.Println("angka ==", angka)
+
+	/* print dari subslice yng dimulai dari index 1 */
 	fmt.Println("angka[1:4] ==", angka[1:4])
 
-  fmt.Println("angka[4:] ==", angka[4:])
+	fmt.Println("angka[4:] ==", angka[4:])
 
-  angka1 := make([]int, 0,5)
-  printSlice(angka1)
+	angka1 := make([]int, 0, 5)
+	printSlice(angka1)
 
-  angka2 := angka[2:]
-  PrintSlice(angka2)
+	angka2 := angka[2:]
+	printSlice(angka2)
 
-  angka3 := angka[2:5]
-  printSlice(angka3)
+	angka3 := angka[2:5]
+	printSlice(angka3)
 }
-func printSlice(x []int){
-  fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
+func printSlice(x []int) {
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
 }
