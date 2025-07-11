8bdcae0b974a44c7bc317e5e0c92603ab25fdd62
ahmad alfandi <alfandi0857@gmail.com>
2025-07-12 02:20:35 +0700

    chore: menambahkan deskripsi fungsi dengan bahasa indonesia

--- tests/test_utils.go
+++ tests/test_utils.go
@@ -6,7 +6,8 @@ import (
 )
 
 // assert fails the test if the condition is false.
-func assert(tb testing.TB, condition bool, msg string, v ...any) {
+// assert: Gagal jika kondisi yang diberikan bernilai false (tidak terpenuhi).
+func Assert(tb testing.TB, condition bool, msg string, v ...any) {
 	tb.Helper()
 	if !condition {
 		tb.Fatalf(msg, v...)
@@ -14,7 +15,8 @@ func assert(tb testing.TB, condition bool, msg string, v ...any) {
 }
 
 // ok fails the test if an err is not nil.
-func ok(tb testing.TB, err error) {
+// ok: Gagal jika error tidak bernilai nil (artinya terjadi kesalahan).
+func OK(tb testing.TB, err error) {
 	tb.Helper() // print runtime caller err
 	if err != nil {
 		tb.Fatalf("unexpected error: %s", err.Error())
@@ -22,7 +24,8 @@ func ok(tb testing.TB, err error) {
 }
 
 // equals fails the test if exp is not equal to act.
-func equals(tb testing.TB, exp, act any) {
+// equals: Gagal jika nilai yang diharapkan (exp) tidak sama dengan nilai aktual (act).
+func Equals(tb testing.TB, exp, act any) {
 	tb.Helper()
 	if !reflect.DeepEqual(exp, act) {
 		tb.Fatalf("exp: %#v\n\n\tgot: %#v", exp, act)
